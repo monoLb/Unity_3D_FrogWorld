1ba4ea865c8c736477da0e4baa0d505ecdb05dbc
mono <monomistake@gmail.com>
2025-05-14 20:39:31 +0800

    Rewrite the code using the state pattern

--- Assets/Scripts/Core/PlacementSystem.cs
+++ Assets/Scripts/Core/PlacementSystem.cs
@@ -6,8 +6,7 @@ using UnityEngine.Serialization;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject mouseIndicator;
+    
 
     // [SerializeField]
     // private GameObject BuildIndicator;
@@ -19,35 +18,33 @@ public class PlacementSystem : MonoBehaviour
     private Grid grid;
     
     //从UI点击获取数据
-    private ObjectData currentObjData;
- 
-
-    [SerializeField]
-    private AudioClip falsePlaceAudio,truePlaceAudio;
-
-
+    // private ObjectData currentObjData;
+    //数据传递，已由PlacemenState获取
+   
     //放置的父物体位置
     [SerializeField] private Transform _placementTransParent;
-
-    //建造模式
-    public bool isPlacing;
+    
 
     //不同种类 创建不同的类方便管理
     [field: SerializeField]
     public GridData floorGrid,furnitureGrid;
     //把创建的家具保存在列表里
-    private List<GameObject> objectList=new();
+    // private List<GameObject> objectList=new(); 新脚本代替
+    
     //能否建造则显示不同的指示器颜色
     // public Renderer _buildIndicatorRenderer;
     //在其他脚本中调用
     [SerializeField]
     private PreviewSystem previewSystem;
     
-    //实时检测能否被创建
-    private bool buildValidity;
     
-    //优化
+    //优化：搭建后不检测原位置
     private Vector3Int lastPosition;
+ 
+    //使用接口
+    IBuildingState buildingState;
+    
+    [SerializeField] private ObjectPlacer placer;
     
     private void Start()
     {
@@ -62,47 +59,64 @@ public class PlacementSystem : MonoBehaviour
     
     private void Update()
     {
+        if(buildingState==null)
+            return;
+        
         var mousePos = inputManager.GetSelectedMapPosition();
-        mouseIndicator.transform.position = mousePos;
-        if(isPlacing)
+        var gridPos = grid.WorldToCell(mousePos);
+        
+        Debug.Log(gridPos+"ss"+lastPosition);
+        if (lastPosition != gridPos)
         {
-            var gridPos = grid.WorldToCell(mousePos);
-
-            if (lastPosition != gridPos)
-            {
-                //实时监测能否被建造
-                buildValidity=CheckBuildValidity(gridPos, currentObjData.Size);
-                //改变颜色,位置
-                previewSystem.UpdatePosition(gridPos,buildValidity);
-            }
-            
+            buildingState.UpdateState(gridPos);
         }
-
+     
     }
 
-    private bool CheckBuildValidity(Vector3Int gridPos, Vector2Int gridSize)
-    {
-        GridData currentGrid = currentObjData.ID == 0 ? floorGrid: furnitureGrid;
-        return currentGrid.CheckIfOccupy(gridPos, gridSize);
-    }
+    
 
     public void SetCurrentObjectData(ObjectData data)
     {
-        currentObjData = data;
-        mouseIndicator.SetActive(true);
-        previewSystem.StopShowingPlacementPreview();
-        lastPosition = Vector3Int.zero;
-        previewSystem.StartShowingPlacementPreview(data.prefab,data.Size);
+        BindInputActions();
+
+        buildingState = new PlacementState(data.ID, grid, previewSystem, data, floorGrid, furnitureGrid, placer);
+        lastPosition=Vector3Int.zero;
     }
-    
+
     public void StartPlacement()
     {
-        mouseIndicator.SetActive(false);
-        // BuildIndicator.SetActive(false);
+        BindInputActions();
+    }
+
+    private void StopPlacement()
+    {
+        if (buildingState==null)
+            return;
+        
+        buildingState.EndState();
+        buildingState = null;
+        Debug.Log("Stop placement");
+
+        previewSystem.StopShowingPlacementPreview();
+        UnbindInputActions();
+    }
+
+    private void BindInputActions()
+    {
+        inputManager.OnClicked -= PlaceStucture;
+        inputManager.OnEsc -= StopPlacement;
         inputManager.OnClicked += PlaceStucture;
         inputManager.OnEsc += StopPlacement;
     }
 
+    private void UnbindInputActions()
+    {
+        inputManager.OnClicked -= PlaceStucture;
+        inputManager.OnEsc -= StopPlacement;
+    }
+
+
+    // ReSharper disable Unity.PerformanceAnalysis
     private void PlaceStucture()
     {
         if (inputManager.IsPointerOverUI())
@@ -115,51 +129,10 @@ public class PlacementSystem : MonoBehaviour
 
         if(lastPosition == gridPos)
             return;
-        if (currentObjData != null)
-        {
-            
-            //如果无法被建造则返回
-            if (!buildValidity)
-            {
-                AudioManager.Instance.PlaySound(falsePlaceAudio,Camera.main.transform.position,1);
-                return;
-            }
-            
-            if (currentObjData.prefab)
-            {
-                // var objPos=grid.CellToWorld(gridPos); 
-                //生成对象
-                GameObject newObj=Instantiate(currentObjData.prefab,_placementTransParent);
-                //修改位置
-                newObj.transform.position = gridPos;
-                //保存在表里
-                objectList.Add(newObj);
-                //根据是否为地板保存在不同的GridData中
-                GridData selectedObj=currentObjData.ID==0?floorGrid:furnitureGrid;
-                selectedObj.AddObjectAt(gridPos,currentObjData.Size,currentObjData.ID,objectList.Count-1);
-                Debug.Log("ss");
-                AudioManager.Instance.PlaySound(truePlaceAudio,Camera.main.transform.position,1);
-                lastPosition=gridPos;
-                
-            }
-        }
-        else
-        {
-            AudioManager.Instance.PlaySound(falsePlaceAudio,Camera.main.transform.position,1);
-        }
-    
+        
+        lastPosition=gridPos;
+        
+        buildingState.OnState(gridPos);
     }
-    
-
-    private void StopPlacement()
-    {
-        isPlacing = false;
-        currentObjData = null;
-        Debug.Log("Stop placement");
-        previewSystem.StopShowingPlacementPreview();
-        inputManager.OnClicked -= PlaceStucture;
-        inputManager.OnEsc -= StopPlacement;
-    }
-
     
 }
